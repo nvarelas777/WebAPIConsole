304ad5d059a16bd581e5934074fb0fe975f8b431
Nick <takethisjaguar@gmail.com>
2018-01-20 14:14:27 -0800

    firstcommit

---
+++ Desktop/WebApiDemo1/WebApiDemo1/Controllers/api/StudentController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo1.Models;
+
+namespace WebApiDemo1.Controllers
+{
+    public class StudentController : ApiController
+    {
+
+        public StudentController()
+        {
+        }
+
+        public IHttpActionResult GetAllStudents(bool includeAddress = false)
+        {
+            IList<StudentViewModel> students = null;
+
+            using (var ctx = new SchoolDBEntities3())
+            {
+                students = ctx.Students.Include("StudentAddress").Select(s => new StudentViewModel()
+                {
+                    StudentID = s.StudentID,
+                    StudentName = s.StudentName,
+                    Address = s.StudentAddress == null || includeAddress == false ? null : new AddressViewModel()
+                    {
+                        StudentId = s.StudentAddress.StudentID,
+                        Address1 = s.StudentAddress.Address1,
+                        Address2 = s.StudentAddress.Address2,
+                        City = s.StudentAddress.City,
+                        State = s.StudentAddress.State
+                    }
+                }).ToList<StudentViewModel>();
+            }
+
+
+            if (students == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(students);
+
+        }
+
+        public IHttpActionResult GetStudentById(int id)
+        {
+            StudentViewModel student = null;
+
+            using (var ctx = new SchoolDBEntities3())
+            {
+                student = ctx.Students.Include("StudentAddress")
+                    .Where(s => s.StudentID == id)
+                    .Select(s => new StudentViewModel()
+                    {
+                        StudentID = s.StudentID,
+                        StudentName = s.StudentName
+                    }).FirstOrDefault<StudentViewModel>();
+            }
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        public IHttpActionResult GetAllStudents(string name)
+        {
+            IList<StudentViewModel> students = null;
+
+            using (var ctx = new SchoolDBEntities3())
+            {
+                students = ctx.Students.Include("StudentAddress")
+                    .Where(s => s.StudentName.ToLower() == name.ToLower())
+                    .Select(s => new StudentViewModel()
+                    {
+                        StudentID = s.StudentID,
+                        StudentName = s.StudentName,                       
+                        Address = s.StudentAddress == null ? null : new AddressViewModel()
+                        {
+                            StudentId = s.StudentAddress.StudentID,
+                            Address1 = s.StudentAddress.Address1,
+                            Address2 = s.StudentAddress.Address2,
+                            City = s.StudentAddress.City,
+                            State = s.StudentAddress.State
+                        }
+                    }).ToList<StudentViewModel>();
+            }
+
+
+            if (students.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(students);
+
+        }
+
+        public IHttpActionResult GetAllStudentsInSameStandard(int standardId)
+        {
+            IList<StudentViewModel> students = null;
+
+            using (var ctx = new SchoolDBEntities3())
+            {
+                students = ctx.Students.Include("StudentAddress").Include("Standard").Where(s => s.StandardId == standardId)
+                            .Select(s => new StudentViewModel()
+                            {
+                                StudentID = s.StudentID,
+                                StudentName = s.StudentName,
+                                Address = s.StudentAddress == null ? null : new AddressViewModel()
+                                {
+                                    StudentId = s.StudentAddress.StudentID,
+                                    Address1 = s.StudentAddress.Address1,
+                                    Address2 = s.StudentAddress.Address2,
+                                    City = s.StudentAddress.City,
+                                    State = s.StudentAddress.State
+                                },
+                                Standard = new StandardViewModel()
+                                {
+                                    StandardId = s.Standard.StandardId,
+                                    Name = s.Standard.StandardName
+                                }
+                            }).ToList<StudentViewModel>();
+            }
+
+
+            if (students.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(students);
+        }
+
+        public IHttpActionResult PostNewStudent(StudentViewModel student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new SchoolDBEntities3())
+            {
+                ctx.Students.Add(new Student()
+                {
+                    StudentID = student.StudentID,
+                    StudentName = student.StudentName,
+                });
+
+                ctx.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(StudentViewModel student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+            using (var ctx = new SchoolDBEntities3())
+            {
+                var existingStudent = ctx.Students.Where(s => s.StudentID == student.StudentID)
+                                                        .FirstOrDefault<Student>();
+
+                if (existingStudent != null)
+                {
+                    existingStudent.StudentName = student.StudentName;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Not a valid student id");
+
+            using (var ctx = new SchoolDBEntities3())
+            {
+                var student = ctx.Students
+                    .Where(s => s.StudentID == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(student).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Desktop/WebApiDemo1/WebApiDemo1/Models/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo1.Models
+{
+    public class StudentViewModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+
+        public AddressViewModel Address { get; set; }
+        public StandardViewModel Standard { get; set; }
+    }
+}
